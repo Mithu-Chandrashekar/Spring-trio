73a0e22dec935d4edfb540b71887db2b5c0c2dc6
CHANDANA-B-R <chandu2004.br99@gmail.com>
2026-06-09 21:35:51 +0530

    updated the lines

--- Register.java
+++ Register.java
@@ -1,7 +1,8 @@
-public class Register{
-    public static void main(String[] args)
-    {   System.out.println("Hello chandana change this mithu to args");
-        System.out.println(10*20);
-       
+public class Register {
+    public static void main(String[] args) {
+        System.out.println("Hello all ");
+        int a = 90;
+        System.out.println("orange");
+
     }
 }
