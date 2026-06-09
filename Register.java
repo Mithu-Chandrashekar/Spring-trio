08227765413d25eaada6212edd9f041bf940cb66
CHANDANA-B-R <chandu2004.br99@gmail.com>
2026-06-09 21:42:33 +0530

    chandu updated

--- Register.java
+++ Register.java
@@ -3,6 +3,9 @@ public class Register {
         System.out.println("Hello all ");
         int a = 90;
         System.out.println("Apple");
+    }
 
+    public void doPost() {
+        System.out.println("this is doPost method");
     }
 }
