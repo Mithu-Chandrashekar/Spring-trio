06ff76795d0e74799f26b99a29db9a01c93bdccc
Mithu-Chandrashekar <mithuchandrashekar@gmail.com>
2026-06-09 21:38:05 +0530

    aaple

--- Register.java
+++ Register.java
@@ -2,7 +2,7 @@ public class Register {
     public static void main(String[] args) {
         System.out.println("Hello all ");
         int a = 90;
-        System.out.println("orange");
+        System.out.println("Apple");
 
     }
 }
