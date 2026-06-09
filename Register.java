c3c8c5c6cc3b18a8ee514609917be73a19996110
Mithu-Chandrashekar <mithuchandrashekar@gmail.com>
2026-06-09 21:31:38 +0530

    rr

--- Register.java
+++ Register.java
@@ -1,7 +1,7 @@
 public class Register{
-    public static void main(String[] chandana)
-    {   System.out.println("Hello mithu change this chandana to args");
-        System.out.println("write a logic to multiply two numbers");
+    public static void main(String[] args)
+    {   System.out.println("Hello chandana change this mithu to args");
+        System.out.println(10*20);
        
     }
 }
